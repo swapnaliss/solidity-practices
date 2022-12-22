876989d1ce0816c9c36a225fcd6774f6575af8c7
swapnaliss <swapnalisbhargude123@gmail.com>
2022-12-22 21:07:26 +0530

    init react setup

---
+++ set-string-dapp/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         setString dApp
+        </p>
+      <p>
+        String is : 
+      </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
