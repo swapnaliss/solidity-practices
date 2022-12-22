1ac63e3d2ff3b77c3b8a964964a22bf6d9ad435d
swapnaliss <swapnalisbhargude123@gmail.com>
2022-12-22 21:24:48 +0530

    truffle init and truffle deploy

---
+++ set-string-dapp/contracts/SetString.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.4.16;
+
+contract SetString {
+    string public storedData;
+
+    function store (string memory data) public{
+        
+        storedData = data;
+        
+    }
+    
+}
---
+++ set-string-dapp/migrations/1671724293_set_string.js
@@ -0,0 +1,5 @@
+const SetString = artifacts.require("SetString");
+
+module.exports = function (deployer) {
+  deployer.deploy(SetString);
+};
