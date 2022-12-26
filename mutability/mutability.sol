4b262432e1a8eb8aeba2ca927b36a0f86aaffd8a
swapnaliss <swapnalisbhargude123@gmail.com>
2022-12-26 19:00:58 +0530

    function Mutability modifier

---
+++ mutability/mutability.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract Mutability{
+
+    uint number;
+
+    function get() view public returns (uint){
+        return number;
+    }
+    function set(uint _number)public{
+        number = _number;
+    }
+    function calculate(uint a , uint b) pure public returns(uint){
+       return a + b;
+    }
+    function sendEther() public payable returns(uint){
+        return address(this).balance;
+    }
+}
