e847c020d6bb394c48afa867d44654de76c14fe2
swapnaliss <swapnalisbhargude123@gmail.com>
2022-12-27 11:56:01 +0530

    added function for add new poll and view poll in smart contract

--- poll-dapp/contracts/Poll.sol
+++ poll-dapp/contracts/Poll.sol
@@ -13,6 +13,21 @@ contract PollContract{
       uint option3votes;
     }
 
-   
-    
+    mapping(string => PollData) public PollDetails;
+
+    function addNewPoll(string memory PollID, string memory question, string memory option1, 
+                           string memory option2,string memory option3)public{
+       PollDetails[PollID].poster = msg.sender;
+       PollDetails[PollID].question = question;
+       PollDetails[PollID].option1  = option1;
+       PollDetails[PollID].option2 = option2;
+       PollDetails[PollID].option3 = option3;
+    }
+
+    function vote(string memory PollID,uint option1,uint option2, uint option3)public{
+      PollDetails[PollID].option1votes += option1;
+      PollDetails[PollID].option2votes += option2;
+      PollDetails[PollID].option3votes += option3;
+    }
+
 }
