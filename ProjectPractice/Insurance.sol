813dc66f7fab9ac4d8d465dcaef32052ca063018
swapnaliss <swapnalisbhargude123@gmail.com>
2023-01-05 22:33:08 +0530

    call contract and define struct for storing the detailes

--- ProjectPractice/Insurance.sol
+++ ProjectPractice/Insurance.sol
@@ -1,2 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
+
+contract CropInsurance {
+    struct Policy{
+        uint id;
+        string name;
+        uint farmerId;
+        string farmerName;
+        uint coveredAmount;
+        uint premiumAmount;
+        uint policyPeriod;
+        
+        }
+}
