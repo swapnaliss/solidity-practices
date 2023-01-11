3e2529d29e2a6226820f8489b267fb8bee0d48d8
swapnaliss <swapnalisbhargude123@gmail.com>
2023-01-11 16:10:45 +0530

    contract created for register land

---
+++ Land/contracts/LandReg.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+contract LandRegistration {
+
+    struct LandDetail {
+        uint surveyNo;
+        string district;
+        string taluk;
+        string village;
+        uint blockNo;
+        uint landValue; // in wei
+        uint area;
+        bool salesStatus;
+    }
+    
+    mapping(uint => LandDetail) LandDetails;
+    
+    uint public registeredLandCount = 0;
+    
+    bool salesStatus;
+    
+    function registerNewLand(uint surveyNo, string memory district, string memory taluk, string memory village, uint blockNo, uint landValue, uint area) public {
+        LandDetails[registeredLandCount] = LandDetail(surveyNo, district, taluk, village, blockNo, landValue, area, false);
+        registeredLandCount += 1;
+    }
+    
+    function getLandDetails(uint landID) view public returns(LandDetail memory) {
+        return LandDetails[landID];
+    }
+}
