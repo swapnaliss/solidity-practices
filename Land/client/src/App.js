71efdc2905d3bee8472afed2b42f10b5f3d407d3
swapnaliss <swapnalisbhargude123@gmail.com>
2023-01-18 10:54:15 +0530

    rendering home page without router

--- Land/client/src/App.js
+++ Land/client/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <p>Hello</p>
-      </header>
+      <HomePage />
     </div>
   );
 }
---
+++ Land/client/src/components/HomePage.js
@@ -0,0 +1,63 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+
+import { BrowserRouter, useNavigate } from "react-router-dom";
+
+export default function HomePage() {
+  const [auth, setAuth] = React.useState(false);
+
+  // const navigate = useNavigate();
+
+  // const redirectHandle = () => {
+  //   navigate('/register');
+  // };
+
+  const handleChange = async (event) => {
+    setAuth(event.target.checked);
+  };
+
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <BrowserRouter
+                    color="default"
+                    checked={auth}
+                    onChange={handleChange}
+                    aria-label="login switch"
+                  />
+                }
+                label={auth ? "Logout" : "Login"}
+              />
+            </FormGroup>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+              LAND MARKETPLACE
+            </Typography>
+            {auth && (
+              <div className="Register">
+                <Typography variant="h5" component="div" sx={{ paddingTop: 1 }}>
+                  Register New Land
+                </Typography>
+                <IconButton size="large" color="inherit">
+                  {/* <AddBoxIcon onClick={redirectHandle} /> */}
+                </IconButton>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+
+      </Box>
+    </div>
+  );
+}
