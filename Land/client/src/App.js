07b7d70ec8bb11863281e2e0e2b38f4551021bf8
swapnaliss <swapnalisbhargude123@gmail.com>
2023-01-20 11:09:42 +0530

    get the deployed contract address from MyContractJSON

--- Land/client/src/App.js
+++ Land/client/src/App.js
@@ -8,7 +8,9 @@ import Web3 from "web3"; //imported web 3 library which is installed
 const ethereum = window.ethereum;  //object can be used to establish the connection between the decentralized application and MetaMask
 
 const MyContractJSON = require("../src/contractJSON/LandSale.json"); // load the artifact file of the deployed contract 
-console.log("MyContractJSON");
+// console.log("MyContractJSON");
+
+const contractAddress = MyContractJSON.networks["5777"].address; // get the deployed contract address from MyContractJSON
 
 function App() {
   return (
