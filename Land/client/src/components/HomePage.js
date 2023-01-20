f1268a22d10c997c3b7f9424d9a5ad6c15711b26
swapnaliss <swapnalisbhargude123@gmail.com>
2023-01-20 12:44:54 +0530

    pass mycontract object to component

--- Land/client/src/components/HomePage.js
+++ Land/client/src/components/HomePage.js
@@ -59,7 +59,7 @@ export default function HomePage(props) {
             )}
           </Toolbar>
         </AppBar>
-        <Marketplace />
+        <Marketplace myContract = {myContract} />   
       </Box>
     </div>
   );
